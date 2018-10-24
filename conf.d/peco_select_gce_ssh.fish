f3f27d7a7bba5c108191003fb503ace104cd4b13
kumanoryo <r-kumano@groovenauts.jp>
2018-10-24 20:45:07 +0900

    Add comment.

--- conf.d/peco_select_gce_ssh.fish
+++ conf.d/peco_select_gce_ssh.fish
@@ -1,9 +1,11 @@
 # Set up an easy-to-use alias.
 set plugin_alias "gssh"
+
+# enable alias.
 alias $plugin_alias "peco_select_gce_ssh"
 
+## disable alias, when uninstall plugin.
 set -l name (basename (status -f) .fish){_uninstall}
-
 function $name
   alias -e $plugin_alias
 end
